d992278fd18b473f16fb3323c75a41d1e68ccb86
techie-aakriti <aakritichaudhary501@gmail.com>
2026-08-17 16:17:45 +0530

    Add files via upload

---
+++ day1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int num1, num2;
+    printf("enter two number:");
+    scanf("%d %d", &num1, &num2);
+    int sum = num1 + num2;
+    printf("sum = %d\n", sum);
+
+    return 0;
+}
