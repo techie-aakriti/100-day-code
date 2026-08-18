fe63ccaa1974ad371338c3942c6b7078e1071678
techie-aakriti <aakritichaudhary501@gmail.com>
2026-08-18 21:37:35 +0530

    Replace old code in day8_q2.c with new implementation (remove previous code)

--- day8_q2.c
+++ day8_q2.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
-
-int main(void) {
+int main()
+{
     int num1, num2, num3;
+
     printf("Enter three numbers: ");
-    if (scanf("%d %d %d", &num1, &num2, &num3) != 3) {
-        printf("Invalid input\n");
-        return 1;
-    }
+    scanf("%d %d %d", &num1, &num2, &num3);
+
     if (num1 >= num2 && num1 >= num3)
         printf("Largest number is %d\n", num1);
     else if (num2 >= num1 && num2 >= num3)
         printf("Largest number is %d\n", num2);
     else
         printf("Largest number is %d\n", num3);
+
     return 0;
 }
