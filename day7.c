33a1b21d66a1269aaf0e8a20d6a82aa2f81655ae
techie-aakriti <aakritichaudhary501@gmail.com>
2026-08-18 11:21:25 +0530

    Restore day7.c with leap year program

--- day7.c
+++ day7.c
@@ -1,1 +1,16 @@
-// File cleared
+#include <stdio.h>
+int main()
+{
+    int year;
+    printf("enter year:");
+    scanf("%d", &year);
+    if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0))
+    {
+        printf("leap year");
+    }
+    else
+    {
+        printf("not leap year");
+    }
+    return 0;
+}
