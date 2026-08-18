4cf6528d52b1f0c56201965a7c59f962b68b4852
techie-aakriti <aakritichaudhary501@gmail.com>
2026-08-18 21:33:09 +0530

    Clean up day8_q2.c - remove unnecessary blank lines

--- day8_q2.c
+++ day8_q2.c
@@ -2,19 +2,16 @@
 
 int main(void) {
     int num1, num2, num3;
-
     printf("Enter three numbers: ");
     if (scanf("%d %d %d", &num1, &num2, &num3) != 3) {
         printf("Invalid input\n");
         return 1;
     }
-
     if (num1 >= num2 && num1 >= num3)
         printf("Largest number is %d\n", num1);
     else if (num2 >= num1 && num2 >= num3)
         printf("Largest number is %d\n", num2);
     else
         printf("Largest number is %d\n", num3);
-
     return 0;
 }
