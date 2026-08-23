bc8f69e0af02f8c390d3435512371b5685e34e7e
techie-aakriti <aakritichaudhary501@gmail.com>
2026-08-23 08:21:58 +0530

    Add files via upload

---
+++ day10.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b, c;
+
+    printf("Enter the three sides of the triangle: ");
+    scanf("%d %d %d", &a, &b, &c);
+
+    if (a + b > c && a + c > b && b + c > a)
+    {
+        if (a == b && b == c)
+        {
+            printf("Equilateral Triangle\n");
+        }
+        else if (a == b || b == c || a == c)
+        {
+            printf("Isosceles Triangle\n");
+        }
+        else
+        {
+            printf("Scalene Triangle\n");
+        }
+    }
+    else
+    {
+        printf("Not a valid triangle\n");
+    }
+
+    return 0;
+}
