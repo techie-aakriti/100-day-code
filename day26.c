b4fc8b3cc1352dff922bdf328c675232e0324fd6
techie-aakriti <aakritichaudhary501@gmail.com>
2026-09-05 12:17:54 +0530

    Add files via upload

---
+++ day26.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+    int n = 5;
+
+    for (int i = n; i >= 1; i--)
+    {
+
+        for (int s = 1; s < i; s++)
+        {
+            printf(" ");
+        }
+        for (int j = i; j <= n; j++)
+        {
+            printf("%d", j);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
---
+++ day26_q2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+    int n = 11;  
+
+    for(int i = 1; i <= n; i++) {
+        printf("*\n");
+    }
+
+    return 0;
+}
