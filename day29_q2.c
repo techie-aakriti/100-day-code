5252ff05e414a033265c1213a12b8119cef5e149
techie-aakriti <aakritichaudhary501@gmail.com>
2026-09-07 10:16:31 +0530

    Add files via upload

---
+++ day29_q2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main() {
+    int n, i;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d elements:\n", n);
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int max = arr[0], min = arr[0];
+    for(i = 1; i < n; i++) {
+        if(arr[i] > max)
+            max = arr[i];
+        if(arr[i] < min)
+            min = arr[i];
+    }
+
+    printf("Maximum = %d\n", max);
+    printf("Minimum = %d\n", min);
+    return 0;
+}
